40dab410e805bce6711ab4e57bbc69a93e9e2bce
Lukasz Towarek <lukasz.towarek@gmail.com>
2021-04-26 19:50:40 +0000

    Deploy to Heroku

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.9-alpine AS build-python
+RUN apk update \
+    && apk add gcc postgresql-dev musl-dev
+COPY ./requirements.txt /
+RUN pip wheel --no-cache-dir --no-deps --wheel-dir /wheels/ -r requirements.txt
+
+FROM python:3.9-alpine
+RUN addgroup -S app && adduser -S app -G app
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+RUN apk update \
+    && apk add postgresql-libs
+COPY --from=build-python /wheels/ /wheels/
+RUN pip install --no-cache /wheels/*
+WORKDIR /code/
+COPY --chown=app:app ./needlimit/ /code/
+RUN python manage.py collectstatic --noinput
+USER app
+CMD gunicorn needlimit.wsgi:application --bind 0.0.0.0:$PORT
--- needlimit/needlimit/settings.py
+++ needlimit/needlimit/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -44,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -83,6 +86,12 @@ DATABASES = {
     }
 }
 
+DATABASE_URL = os.environ.get("DATABASE_URL")
+db_from_env = dj_database_url.config(
+    default=DATABASE_URL, conn_max_age=500, ssl_require=True
+)
+DATABASES["default"].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -121,6 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / "static"]
+else:
+    STATIC_ROOT = BASE_DIR / "static"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
